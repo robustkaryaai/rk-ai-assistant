67092f18c0d666002f5c7d88f36a5a2f9b093f56
ARKIS DEVELOPER <robustkaryaai@gmail.com>
2026-03-08 23:35:58 +0530

    cleanup: remove debug, test, and troubleshooting scripts

--- cleanup_alsa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Remove lingering ALSA/BlueALSA configs
-sudo rm -f /etc/asound.conf
-sudo rm -f /home/raspberrypi/.asoundrc
-sudo rm -f /usr/share/alsa/alsa.conf.d/*bluealsa*
-# Also remove any pulse cookie issues?
-rm -f ~/.config/pulse/cookie
-echo "ALSA cleanup complete."
